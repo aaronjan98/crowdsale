3e0120381c3f774f10e4d9671af1f7a6755f8317
aaronjan98 <aaronjan98@gmail.com>
2022-11-21 14:27:40 -0800

    create Token contract and tests

--- contracts/Crowdsale.sol
+++ contracts/Crowdsale.sol
@@ -1,6 +1,14 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.17;
 
+import 'hardhat/console.sol';
+import './Token.sol';
+
 contract Crowdsale {
     string public name = 'Crowdsale';
+    Token public token;
+
+    constructor(Token _token) {
+        token = _token;
+    }
 }
--- test/Crowdsale.js
+++ test/Crowdsale.js
@@ -6,16 +6,23 @@ const tokens = n => {
 }
 
 describe('Crowdsale', () => {
-  let crowdsale
+  let crowdsale, token
 
   beforeEach(async () => {
     const Crowdsale = await ethers.getContractFactory('Crowdsale')
-    crowdsale = await Crowdsale.deploy()
+    const Token = await ethers.getContractFactory('Token')
+
+    token = await Token.deploy('Jan Token', 'PHI', '1000000')
+    crowdsale = await Crowdsale.deploy(token.address)
   })
 
   describe('Deployment', () => {
     it('has correct name', async () => {
       expect(await crowdsale.name()).to.eq('Crowdsale')
+    })
+
+    it('returns token address', async () => {
+      expect(await crowdsale.token()).to.eq(token.address)
     })
   })
 })

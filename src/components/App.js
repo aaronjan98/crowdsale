9f08bcc73f3246ecc5a522a03df5be38d75a3cdd
aaronjan98 <aaronjan98@gmail.com>
2022-11-23 15:32:04 -0800

    create Navbar in Navigation component

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import Container from 'react-bootstrap/Container'
+
+// Components
+import Navigation from './Navigation.js'
+
+function App() {
+  return (
+    <Container>
+      <Navigation />
+    </Container>
+  )
+}
+
+export default App

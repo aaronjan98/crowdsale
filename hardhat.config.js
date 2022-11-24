50a5c65d9046ee32c7d61b2e6bae7eb91b662806
aaronjan98 <aaronjan98@gmail.com>
2022-11-23 16:28:36 -0800

    load accounts from ethereum connection

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,11 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomicfoundation/hardhat-toolbox')
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
-};
+  solidity: '0.8.17',
+  networks: {
+    ganache: {
+      url: 'http://127.0.0.1:3333',
+    },
+  },
+}
--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,33 @@
+import { useEffect, useState } from 'react'
 import Container from 'react-bootstrap/Container'
+import { ethers } from 'ethers'
 
 // Components
-import Navigation from './Navigation.js'
+import Navigation from './Navigation'
+import Info from './Info'
 
 function App() {
+  const [account, setAccount] = useState(null)
+
+  const loadBlockchainData = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+
+    const accounts = await window.ethereum.request({
+      method: 'eth_requestAccounts',
+    })
+    const account = ethers.utils.getAddress(accounts[0])
+    setAccount(account)
+  }
+
+  useEffect(() => {
+    loadBlockchainData()
+  })
+
   return (
     <Container>
       <Navigation />
+      <hr />
+      {account && <Info account={account} />}
     </Container>
   )
 }

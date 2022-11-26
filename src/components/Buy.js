cbb36fc55466b4f02d1389f20b0e3d1050adcd8e
aaronjan98 <aaronjan98@gmail.com>
2022-11-25 16:04:06 -0800

    implement buyHandler

--- src/components/Buy.js
+++ src/components/Buy.js
@@ -1,15 +1,42 @@
+import { useState } from 'react'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Spinner from 'react-bootstrap/Spinner'
+import { ethers } from 'ethers'
 
 const Buy = ({ provider, price, crowdsale, setIsLoading }) => {
+  const [amount, setAmount] = useState(0)
+
+  const buyHandler = async e => {
+    e.preventDefault()
+    console.log('buying tokens...', amount)
+
+    const signer = await provider.getSigner()
+
+    // need to calculate the required ETH in order to buy the tokens...
+    const value = ethers.utils.parseUnits((amount * price).toString(), 'ether')
+    const formattedAmount = ethers.utils.parseUnits(amount.toString(), 'ether')
+
+    const transaction = await crowdsale
+      .connect(signer)
+      .buyTokens(formattedAmount, { value: value })
+    await transaction.wait()
+  }
+
   return (
-    <Form style={{ maxWidth: '800px', margin: '50px auto' }}>
+    <Form
+      onSubmit={buyHandler}
+      style={{ maxWidth: '800px', margin: '50px auto' }}
+    >
       <Form.Group as={Row}>
         <Col>
-          <Form.Control type="number" placeholder="Enter amount" />
+          <Form.Control
+            type="number"
+            placeholder="Enter amount"
+            onChange={e => setAmount(e.target.value)}
+          />
         </Col>
         <Col className="text-center">
           <Button variant="primary" type="submit" style={{ width: '100%' }}>
